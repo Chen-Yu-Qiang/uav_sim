9fccb37c864454a3fa4c980d6612ece9dabb0cbd
chen yu qiang <a@example.com>
2020-10-04 15:01:17 +0800

    estimatePoseBoard整體

--- src/cvqiao/src/image_trans.py
+++ src/cvqiao/src/image_trans.py
@@ -8,6 +8,8 @@ import cv2
 import Queue
 import threading
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import PoseStamped
+import math
 from geometry_msgs.msg import Point
 from cv2 import aruco
 import time
@@ -20,6 +22,42 @@ def ROSdata2CV(data):
     except CvBridgeError as e:
         print(e)
         return
+
+def quaternion_from_matrix(matrix):
+    T1=np.append(matrix,np.array([[0,0,0]]),axis=0)
+    T2=np.append(T1,np.array([[0],[0],[0],[1]]),axis=1)
+    
+
+    q = np.empty((4, ), dtype=np.float64)
+    M = np.array(T2, dtype=np.float64, copy=False)[:4, :4]
+    t = np.trace(M)
+    if t > M[3, 3]:
+        q[3] = t
+        q[2] = M[1, 0] - M[0, 1]
+        q[1] = M[0, 2] - M[2, 0]
+        q[0] = M[2, 1] - M[1, 2]
+    else:
+        i, j, k = 0, 1, 2
+        if M[1, 1] > M[0, 0]:
+            i, j, k = 1, 2, 0
+        if M[2, 2] > M[i, i]:
+            i, j, k = 2, 0, 1
+        t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+        q[i] = t
+        q[j] = M[i, j] + M[j, i]
+        q[k] = M[k, i] + M[i, k]
+        q[3] = M[k, j] - M[j, k]
+    q *= 0.5 / math.sqrt(t * M[3, 3])
+    return q
+
+def qmulq(q1,q2):
+    q3=[0,0,0,0]
+
+    q3[0]=q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3]
+    q3[1]=q1[0]*q2[1]+q1[1]*q2[0]+q1[2]*q2[3]-q1[3]*q2[2]
+    q3[2]=q1[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1]
+    q3[3]=q1[0]*q2[3]+q1[1]*q2[2]-q1[2]*q2[1]+q1[3]*q2[0]
+    return q3
 
 
 N_worker = 1
@@ -60,7 +98,7 @@ class Worker(threading.Thread):
         self.output_queue = _output_queue
 
     def run(self):
-        global last_frame
+        global last_frame,cam_pub
         while 1:
             while self.input_queue.qsize() == 0:
                 pass
@@ -103,6 +141,30 @@ class Worker(threading.Thread):
                                aruco.DICT_ARUCO_ORIGINAL),
                                board_ids)
             retval, rvec, tvec =   aruco.estimatePoseBoard(corners,ids, board, camera_matrix,dist_coeffs)
+            if retval == 0:
+                continue
+            a=rvec
+            b=tvec
+            rr, _ = cv2.Rodrigues(np.array([a[0][0], a[1][0], a[2][0]]))
+            tt = np.array([b[0][0], b[1][0], b[2][0]+0.21])
+            cam_r = rr.transpose()
+            cam_t = -cam_r.dot(tt)
+            cam_x = cam_t[0]
+            cam_y = cam_t[1]
+            cam_z = cam_t[2]
+            cam_q=quaternion_from_matrix(cam_r)
+            tq=[-0.5, 0.5, 0.5, 0.5]
+            q3=qmulq(tq,cam_q)
+            pubmsg=PoseStamped()
+            pubmsg.pose.position.x=cam_x
+            pubmsg.pose.position.y=cam_y
+            pubmsg.pose.position.z=cam_z
+            pubmsg.pose.orientation.w=q3[3]
+            pubmsg.pose.orientation.x=q3[0]
+            pubmsg.pose.orientation.y=q3[1]
+            pubmsg.pose.orientation.z=q3[2]
+            pubmsg.header.frame_id="world"
+            cam_pub.publish(pubmsg)
 
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
@@ -141,7 +203,7 @@ def detect(img):
 
 rospy.init_node('tim', anonymous=True)
 ic = image_converter()
-
+cam_pub=rospy.Publisher("cam_from_all_marker",PoseStamped,queue_size=1)
 
 try:
     rospy.spin()
